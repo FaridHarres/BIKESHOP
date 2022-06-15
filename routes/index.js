2ce9873c1f919d4084407fd4fb46e011f5ad2fe2
fafa-dev <91870965+fafa-dev@users.noreply.github.com>
2022-06-15 15:03:49 +0200

    add http get and post

--- routes/index.js
+++ routes/index.js
@@ -7,93 +7,52 @@ var dataBike=[
     image: "/images/bike-1.jpg",
     nom: "BIKE 045",
     prix : 679,
-    quantity: 0 
+    quantity: 1 
   },
   {
     id : 2,
     image: "/images/bike-2.jpg",
     nom: "ZOOK O7",
     prix : 799,
-    quantity: 0   
+    quantity: 1 
   },
   {
     id : 3,
     image: "/images/bike-3.jpg",
     nom: "LIKO 89",
     prix : 679,
-    quantity: 0   
+    quantity: 1 
   },
   {
     id : 4,
     image: "/images/bike-4.jpg",
     nom: "GEW08",
     prix : 1249,
-    quantity: 0   
+    quantity: 1 
   },
   {
     id : 5,
     image: "/images/bike-5.jpg",
     nom: "KIWIT",
     prix : 899,
-    quantity: 0   
+    quantity: 1 
   },
   {
     id : 6,
     image: "/images/bike-6.jpg",
     nom: "NASAY",
     prix : 1399,
-    quantity: 0   
+    quantity: 1 
   },
   
 ]
 
 var dataCardBike = [
-  {
-    image: "/images/bike-1.jpg",
-    nom: "BIKE 045",
-    prix : 679,
-    quantity: 1 
-  },
-  {
-    image: "/images/bike-2.jpg",
-    nom: "ZOOK O7",
-    prix : 799,
-    quantity: 3   
-  },
-  {
-    image: "/images/bike-3.jpg",
-    nom: "LIKO 89",
-    prix : 679,
-    quantity: 1   
-  },
-  {
-    image: "/images/bike-4.jpg",
-    nom: "GEW08",
-    prix : 1249,
-    quantity: 1   
-  },
-  {
-    image: "/images/bike-5.jpg",
-    nom: "KIWIT",
-    prix : 899,
-    quantity: 1   
-  },
-  {
-    image: "/images/bike-6.jpg",
-    nom: "NASAY",
-    prix : 1399,
-    quantity: 1   
-  },
  
 ]
 
-var totalCmd = 0
-for (var i=0; i<dataCardBike.length; i++){  
-  totalCmd += (dataCardBike[i].prix * dataCardBike[i].quantity)
-  
- } 
 
- console.log(totalCmd)
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -101,8 +60,50 @@ router.get('/', function(req, res, next) {
 });
 
 
-
+var totalCmd = 0
 router.get('/shop', function(req, res, next) {
+  //console.log(req.query)
+  dataCardBike.push(req.query)
+  totalCmd = 0
+  for (var val of dataCardBike){ 
+
+    totalCmd = totalCmd  + (val.prix *val.quantity)
+   } 
+  res.render('shop', {dataCardBike, totalCmd});
+});
+
+
+var totalCmd = 0
+router.get('/delete', function(req, res, next) {
+  //console.log("query", req.query)
+      dataCardBike.splice(req.query.index, 1)
+      totalCmd = 0
+      for (var val of dataCardBike){  
+        totalCmd = totalCmd  + (val.prix * val.quantity)
+        //console.log(totalCmd)
+      } 
+
+  res.render('shop', {dataCardBike, totalCmd});
+});
+
+
+
+
+//ajout de la quantité
+
+router.post('/update-shop', function(req, res, next) {
+  console.log("ajout",req.body.quantity)
+  let indexQte = req.body.index 
+    dataCardBike[indexQte].quantity = req.body.quantity
+    totalCmd=0
+    for(var i=0; i<dataCardBike.length; i++){
+      totalCmd += (dataCardBike[i].quantity * dataCardBike[i].prix)
+
+    }
+
+
+
+  
   res.render('shop', {dataCardBike, totalCmd});
 });
 
